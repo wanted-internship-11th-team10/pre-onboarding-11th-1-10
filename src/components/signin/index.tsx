7566b200061818217844ef6f5c2d6225d1cbf823
Inhu Jo <inhudev@gmail.com>
2023-06-28 00:22:37 +0900

    feat: 로그인 폼에 useInputValue 훅 적용

--- src/components/signin/index.tsx
+++ src/components/signin/index.tsx
@@ -1,9 +1,14 @@
+import useInputValue from '../../hooks/useInputValue';
+
 export default function SignInForm() {
+  const [email, handleEmailChange] = useInputValue();
+  const [password, handlePasswordChange] = useInputValue();
+
   return (
     <>
       <form>
-        <input type="email" placeholder="이메일" />
-        <input type="password" placeholder="비밀번호" />
+        <input type="email" placeholder="이메일" value={email} onChange={handleEmailChange} />
+        <input type="password" placeholder="비밀번호" value={password} onChange={handlePasswordChange} />
         <button type="submit">로그인</button>
       </form>
       <div>
