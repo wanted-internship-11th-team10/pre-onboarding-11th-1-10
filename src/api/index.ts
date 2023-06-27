87124946724f9bcfbcfcd04369f5150044e7de79
Inhu Jo <inhudev@gmail.com>
2023-06-28 00:47:24 +0900

    feat: axios 인터셉터 설정

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,8 +1,17 @@
 import axios from 'axios';
+import useToken from '../hooks/useToken';
 
 export const apiInstance = axios.create({
   baseURL: 'https://www.pre-onboarding-selection-task.shop',
   headers: {
     'Content-Type': 'application/json',
   },
+});
+
+apiInstance.interceptors.request.use((config) => {
+  const { token } = useToken();
+
+  if (token) config.headers['Authorization'] = `Bearer ${token}`;
+
+  return config;
 });
