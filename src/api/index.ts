c8061725e4c21f2868a590a3242a6258d9c1ebb5
Inhu Jo <inhudev@gmail.com>
2023-06-28 00:37:40 +0900

    feat: axios 인스턴스 객체 정의

---
+++ src/api/index.ts
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+export const apiInstance = axios.create({
+  baseURL: 'https://www.pre-onboarding-selection-task.shop',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
