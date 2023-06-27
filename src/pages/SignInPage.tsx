aa09dbd62f6232938893c8e9f976773a35f99e1d
Inhu Jo <inhudev@gmail.com>
2023-06-28 00:05:12 +0900

    feat: 페이지 생성

---
+++ src/pages/SignInPage.tsx
@@ -0,0 +1,7 @@
+export default function SignInPage() {
+  return (
+    <div>
+      <h1>로그인</h1>
+    </div>
+  );
+}
---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,7 @@
+export default function TodoPage() {
+  return (
+    <div>
+      <h1>TODO</h1>
+    </div>
+  );
+}
