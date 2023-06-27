07baf3ba5c55b5f49f061a7b50df181c545a3703
Inhu Jo <inhudev@gmail.com>
2023-06-28 00:24:10 +0900

    feat: 로그인 폼 submit 핸들러 정의

--- src/components/signin/index.tsx
+++ src/components/signin/index.tsx
@@ -1,12 +1,18 @@
+import { FormEvent } from 'react';
 import useInputValue from '../../hooks/useInputValue';
 
 export default function SignInForm() {
   const [email, handleEmailChange] = useInputValue();
   const [password, handlePasswordChange] = useInputValue();
 
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    alert(email + ' ' + password);
+  };
+
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input type="email" placeholder="이메일" value={email} onChange={handleEmailChange} />
         <input type="password" placeholder="비밀번호" value={password} onChange={handlePasswordChange} />
         <button type="submit">로그인</button>
