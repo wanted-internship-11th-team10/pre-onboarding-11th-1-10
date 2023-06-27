8f8a5669628442980a36f5ddd2d3166080ebbc4e
Inhu Jo <inhudev@gmail.com>
2023-06-28 00:07:05 +0900

    feat: 페이지별 라우팅 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div>App</div>;
-}
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import SignInPage from './pages/SignInPage';
+import SignUpPage from './pages/SignUpPage';
+import TodoPage from './pages/TodoPage';
 
-export default App;
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/signin" />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/todo" element={<TodoPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
