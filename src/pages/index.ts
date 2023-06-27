7283e4a90278b3701391b3b5e8a328b006037c41
jhwmac <jkr2505@naver.com>
2023-06-28 00:17:56 +0900

    feat : 기초 디렉토리 구성

---
+++ src/pages/index.ts
@@ -0,0 +1,2 @@
+export {default as Signin} from './signin'
+export {default as Signup} from './signup'
--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,4 +1,5 @@
-const App = () => {
+
+const signin = () => {
   return (
     <div>
       
@@ -6,4 +7,4 @@ const App = () => {
   )
 }
 
-export default App
+export default signin
