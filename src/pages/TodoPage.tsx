0179708f8797504ef10adcdf66aaac5f73166377
sik9252 <sik9252@naver.com>
2023-06-28 22:17:34 +0900

    feat: 전체적인 Todo UI를 보여줄 TodoPage 추가

---
+++ src/pages/TodoPage.tsx
@@ -0,0 +1,14 @@
+/**
+ * 전체적인 Todo 리스트를 보여줄 페이지
+ */
+
+import TodoList from '../components/Todo/TodoList';
+
+export const TodoPage = () => {
+  return (
+    <>
+      <h2>Todo 목록</h2>
+      <TodoList />
+    </>
+  );
+};
