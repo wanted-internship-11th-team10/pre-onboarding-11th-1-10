78fbc7240369b76600abe09e898989e2a803c44e
kimseunggyu <dev.seunggyu@gmail.com>
2023-06-28 10:14:12 +0900

    feat : 로그인 여부에 따른 리다이렉트 처리 구현 - 로그인상태로 /signin 또는 /signup 접속시 /todo 로 리다이렉트 - 비로그인상태로 /todo 접속시 /signin 로 리다이렉트

---
+++ src/pages/PrivateRoutes.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom';
+
+import { useAuth } from '../hooks';
+import { RoutePath } from './Routes';
+
+type PrivateRoutesProps = {
+  redirectPath: RoutePath;
+};
+
+export function PrivateRoutes({ redirectPath }: PrivateRoutesProps) {
+  const { isLogin } = useAuth();
+  return isLogin ? <Outlet /> : <Navigate to={redirectPath} />;
+}
--- src/pages/Routes.tsx
+++ src/pages/Routes.tsx
@@ -1,4 +1,7 @@
-import { Routes as ReactRouterRoutes, Route } from 'react-router-dom';
+import { Routes as ReactRouterRoutes, Route, Navigate } from 'react-router-dom';
+
+import { PrivateRoutes } from './PrivateRoutes';
+import { AuthRedirect } from './AuthRedirect';
 
 import { SignupPage } from './SignupPage';
 import { SigninPage } from './SigninPage';
@@ -6,10 +9,20 @@ import { SigninPage } from './SigninPage';
 export const Routes = () => {
   return (
     <ReactRouterRoutes>
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/signin" element={<SigninPage />} />
-      <Route path="/" element={<div>홈</div>} />
+      {/*  GYU-TODO: 가독성이 안좋아 보임 */}
+      <Route element={<AuthRedirect redirectPath="/todo" />}>
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/signin" element={<SigninPage />} />
+      </Route>
+
+      <Route element={<PrivateRoutes redirectPath="/signin" />}>
+        <Route path="/todo" element={<div>TODO</div>} />
+      </Route>
+
+      <Route path="/" element={<Navigate to="/todo" />} />
       <Route path="*" element={<div>Not Found Page</div>} />
     </ReactRouterRoutes>
   );
 };
+
+export type RoutePath = '/signup' | '/signin' | '/todo';
