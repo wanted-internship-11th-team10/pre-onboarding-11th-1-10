558f909ed3c83c572ab215d4f50f8067535aa35a
kimseunggyu <dev.seunggyu@gmail.com>
2023-06-28 19:18:17 +0900

    feat : todoList 렌더링 구현

--- src/components/todo/TodoList.tsx
+++ src/components/todo/TodoList.tsx
@@ -1,27 +1,9 @@
 import { TodoItem } from './TodoItem';
-import { Todo } from '../../api/todo';
+import { useTodo } from '../../context/todo';
 
-const todos: Todo[] = [
-  {
-    id: 1,
-    todo: 'react',
-    userId: 1,
-    isCompleted: false,
-  },
-  {
-    id: 2,
-    todo: 'typescript',
-    userId: 1,
-    isCompleted: false,
-  },
-  {
-    id: 3,
-    todo: 'nextjs',
-    userId: 1,
-    isCompleted: true,
-  },
-];
 export function TodoList() {
+  const { todos } = useTodo();
+
   return (
     <>
       {todos.map((todo) => (
