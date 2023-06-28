c42784a2dec929c69e9b105ddfc4a026beee181f
kimseunggyu <dev.seunggyu@gmail.com>
2023-06-28 19:18:32 +0900

    feat : todo item 추가 기능 구현

--- src/components/todo/TodoAdder.tsx
+++ src/components/todo/TodoAdder.tsx
@@ -1,13 +1,27 @@
 import { ChangeEvent, FormEvent, useState } from 'react';
 
+import { useTodo } from '../../context/todo';
+
 export function TodoAdder() {
+  const { onTodoAdder } = useTodo();
+
+  const [todo, setTodo] = useState('');
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setTodo(event.target.value);
+  };
+
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (!todo) return;
+
+    onTodoAdder(todo);
+    setTodo('');
+  };
+
   return (
-    <form
-      onSubmit={() => {
-        console.log;
-      }}
-    >
-      <input data-testid="new-todo-input" />
+    <form onSubmit={handleSubmit}>
+      <input data-testid="new-todo-input" value={todo} onChange={handleChange} />
       <button type="submit" data-testid="new-todo-add-button">
         추가
       </button>
