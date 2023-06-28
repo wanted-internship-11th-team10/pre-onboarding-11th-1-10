03282734f3ae09c1debb997f10070f1aa3ed1542
kimseunggyu <dev.seunggyu@gmail.com>
2023-06-28 19:48:20 +0900

    feat : todo item 기능 구현 - ItemViewer   - edit 모드 수정 및 삭제 기능, completed 기능 구현 - ItemEditor   - view 모드 수정 및 todo 텍스트 수정 기능

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -1,9 +1,31 @@
-import { Todo } from '../../api/todo';
+import { useState } from 'react';
+
+import { Todo } from '../../api';
+import { TodoItemViewer } from './TodoItemViewer';
+import { TodoItemEditor } from './TodoItemEditor';
+import styled from 'styled-components';
 
 type TodoItemProps = {
   todo: Todo;
 };
 
 export function TodoItem({ todo }: TodoItemProps) {
-  return <li>{todo.todo}</li>;
+  const [viewMode, setViewMode] = useState(true);
+
+  // GYU-TODO: 선언형이면, react-if 패턴이 더 좋을까!?
+  return (
+    <TodoItemWrapper>
+      {viewMode ? (
+        <TodoItemViewer todo={todo} onChangeEditMode={() => setViewMode(false)} />
+      ) : (
+        <TodoItemEditor todo={todo} onChangeViewMode={() => setViewMode(true)} />
+      )}
+    </TodoItemWrapper>
+  );
 }
+
+const TodoItemWrapper = styled.li`
+  list-style: none;
+  display: flex;
+  gap: 0.5rem;
+`;
